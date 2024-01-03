d23b73cb19265b8c99a77690aa7a705107ea147b
Tibor-Krols <56554490+Tibor-Krols@users.noreply.github.com>
2024-01-03 12:10:28 +0100

    working version of aal atlas

--- utils/cortical_masking.py
+++ utils/cortical_masking.py
@@ -1,13 +1,9 @@
 import nilearn
-from dataset_loader.dataset import LPPDataset
-from dataset_loader.section_participant_base import BaseSectionParticipant
-from nilearn.input_data import NiftiMasker
 import numpy as np
 from nilearn import datasets, image, plotting
 from nilearn.image import resample_img
 import nibabel as nib
 from tqdm import tqdm
-from models.bayesian.fmri_averages_per_participant import load_averages_participant
 import torch
 
 def resample_mask(mask):
@@ -85,72 +81,6 @@ def get_oxford_mask(cortical_regions : list[str],select_all_regions=False):
     # plotting.plot_roi(cortical_mask, title=str(cortical_regions))
     # plotting.show()
     return cortical_mask
-def get_aal_mask_depricated():
-    """needs refinement if used"""
-    # Load AAL atlas provided by Nilearn
-    aal_atlas = datasets.fetch_atlas_aal()
-
-    # AAL atlas filename
-    aal_img = aal_atlas.maps
-    # Load the AAL atlas image
-    img_aal = image.load_img(aal_img)
-
-    # View the labels available in the AAL atlas
-    # print(aal_atlas.labels)
-
-    # Extract the label indices corresponding to the prefrontal cortex
-    # prefrontal_indices = [2, 3, 4, 5, 6]  # Update with the appropriate indices for the prefrontal cortex
-    prefrontal_indices = list(range(16))
-
-    # Create a mask for the prefrontal cortex from AAL atlas
-    prefrontal_mask = image.math_img('img == {}'.format(prefrontal_indices[0]), img=img_aal)
-    for index in prefrontal_indices[1:]:
-        prefrontal_mask = image.math_img('np.logical_or(img == {}, mask)'.format(index), img=img_aal,
-                                         mask=prefrontal_mask)
-
-    # Plot the prefrontal mask
-    plotting.plot_roi(prefrontal_mask, title='Prefrontal Cortex Mask (AAL)')
-    plotting.show()
-    return prefrontal_mask
-def get_aal_mask_v2(cortical_regions : list[str],select_all_regions=False):
-    """Cortical regions need to be part of the atlas """
-
-    # Load Harvard-Oxford cortical probabilistic atlas (50% threshold by default)
-    aal_atlas = datasets.fetch_atlas_aal()
-
-    #select all regions (except background) if specified:
-    if select_all_regions:
-        cortical_regions = aal_atlas.labels
-        cortical_regions.remove('Background')
-    # get index of cortical regions
-    cortical_indices = [aal_atlas.indices[aal_atlas.labels.index(region)] for region in cortical_regions]
-    # cortical_indices = [aal_atlas.labels.index(region) for region in cortical_regions]
-
-    cortical_mask = image.math_img('img == {}'.format(cortical_indices[0]), img=aal_atlas.maps)
-    shape = cortical_mask.shape
-    affine = cortical_mask.affine
-    cortical_mask = cortical_mask.get_fdata()
-    if cortical_regions[0] == 'Background':
-        cortical_mask = 1 - cortical_mask
-    for index in cortical_indices[1:]:
-        if harvard_oxford.labels[index] == 'Background':
-            print('background')
-            cortical_mask += 1- image.math_img('img == {}'.format(index), img=harvard_oxford.maps).get_fdata()
-        else:
-            cortical_mask += image.math_img('img == {}'.format(index), img=harvard_oxford.maps).get_fdata()
-
-
-    # convert to nifti imgage
-    header = nib.nifti1.Nifti1Header()
-    header.set_data_shape(shape)
-    cortical_mask = nib.nifti1.Nifti1Image(cortical_mask, affine=affine, header=header)
-
-
-    cortical_mask = resample_mask(cortical_mask)
-    # Plot the cortical mask from Harvard-Oxford Atlas
-    # plotting.plot_roi(cortical_mask, title=str(cortical_regions))
-    # plotting.show()
-    return cortical_mask
 
 def get_aal_mask(cortical_regions : list[str],select_all_regions=False):
     """Cortical regions need to be part of the atlas """
@@ -167,11 +97,7 @@ def get_aal_mask(cortical_regions : list[str],select_all_regions=False):
     if select_all_regions:
         cortical_regions = aal_atlas.labels
     # get index of cortical regions
-    # cortical_indices = [aal_atlas.labels.index(region) for region in cortical_regions]
     cortical_indices = [aal_atlas.indices[aal_atlas.labels.index(region)] for region in cortical_regions]
-
-    # cortical_mask = image.math_img('img == {}'.format(cortical_indices[0]), img=img_aal)
-
 
     # Create a mask for the prefrontal cortex from AAL atlas
     cortical_mask = image.math_img('img == {}'.format(cortical_indices[0]), img=img_aal)
@@ -179,46 +105,17 @@ def get_aal_mask(cortical_regions : list[str],select_all_regions=False):
     affine = cortical_mask.affine
     cortical_mask = cortical_mask.get_fdata()
     for index in cortical_indices[1:]:
-        # cortical_mask += image.math_img('img == {}'.format(index), img=harvard_oxford.maps).get_fdata()
         cortical_mask += image.math_img('img == {}'.format(index), img=img_aal).get_fdata()
-
-        # cortical_mask = image.math_img('np.logical_or(img == {}, mask)'.format(index), img=img_aal,
-        #                                  mask=cortical_mask)
-
-
-
-    # if cortical_regions[0] == 'Background':
-    #     cortical_mask = 1 - cortical_mask
-    # for index in cortical_indices[1:]:
-    #     cortical_mask += image.math_img('img == {}'.format(index), img=img_aal).get_fdata()
-
-        # Create a mask for the prefrontal cortex from AAL atlas
-        # prefrontal_mask = image.math_img('img == {}'.format(prefrontal_indices[0]), img=img_aal)
-        # for index in prefrontal_indices[1:]:
-        #     prefrontal_mask = image.math_img('np.logical_or(img == {}, mask)'.format(index), img=img_aal,
-        #                                      mask=prefrontal_mask)
-        # Create a mask for the prefrontal cortex from AAL atlas
-        # prefrontal_mask = image.math_img('img == {}'.format(prefrontal_indices[0]), img=img_aal)
-        # for index in prefrontal_indices[1:]:
-        #     prefrontal_mask = image.math_img('np.logical_or(img == {}, mask)'.format(index), img=img_aal,
-        #                                      mask=prefrontal_mask)
-        # if aal_atlas.labels[index] == 'Background':
-        #     print('background')
-        #     cortical_mask += 1- image.math_img('img == {}'.format(index), img=aal_atlas.maps).get_fdata()
-        # else:
-
 
     # convert to nifti imgage
     header = nib.nifti1.Nifti1Header()
     header.set_data_shape(shape)
     cortical_mask = nib.nifti1.Nifti1Image(cortical_mask, affine=affine, header=header)
 
-
+    #reshape mask to mni template format
     cortical_mask = resample_mask(cortical_mask)
-    # Plot the cortical mask from Harvard-Oxford Atlas
-    plotting.plot_roi(cortical_mask, title=str(cortical_regions))
-    plotting.show()
     return cortical_mask
+
 def make_nifti_image_from_numpy(fmri_n):
     affine = np.array(
         [[2., 0., 0., -72.],
